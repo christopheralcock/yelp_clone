96b0cf9349072bf4248411cb93f6dfc0706c1a3c
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-09-02 11:32:43 +0100

    third test pass

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -26,6 +26,8 @@ feature 'restaurants' do
 			visit '/restaurants' 
 			click_link 'Add a restaurant'
 			fill_in 'Name', with: 'KFC'
+			click_button 'Create Restaurant'
+			expect(page).to have_content 'KFC'
 			expect(current_path).to eq '/restaurants'
 		end 
 	end 
